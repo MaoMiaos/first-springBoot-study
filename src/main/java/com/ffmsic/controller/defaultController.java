5edb598b8364d4d0f4b807b1d497850327b6267a
MaoMiaos <1444645996qq.com>
2022-01-01 14:49:12 +0800

    Affair request

--- src/main/java/com/ffmsic/controller/defaultController.java
+++ src/main/java/com/ffmsic/controller/defaultController.java
@@ -1,12 +1,14 @@
 package com.ffmsic.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class defaultController {
     @GetMapping
     public String hello(){
